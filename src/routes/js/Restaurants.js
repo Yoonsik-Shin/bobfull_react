09dd0d8ba08dc63ea382ac97b5ad9a646a192202
tnpfldyd <dkdldhs0322@gmail.com>
2022-12-09 19:29:40 +0900

    feat : Category 추가

--- src/routes/js/Restaurants.js
+++ src/routes/js/Restaurants.js
@@ -2,12 +2,17 @@ import axios from 'axios'
 import React, { useState, useEffect, useCallback } from 'react';
 import '../css/Restaurants.css'
 import { Container } from 'react-bootstrap'
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Location from '../../hooks/useWatchLocation.js'
 import { useInView } from "react-intersection-observer"
 import styled from '../../components/css/Button.module.css';
 import { } from 'react-kakao-maps-sdk'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 function Restaurants() {
+  const code = new URL(window.location.href).searchParams.get('category')
   var baseURL = process.env.REACT_APP_BASE_URL
   const [restaurants, setRestaurants] = useState([])
   const [number, setNumber] = useState(0)
@@ -61,6 +66,7 @@ function Restaurants() {
       url: `${baseURL}/restaurant/`,
       headers: { 'Content-Type': 'application/json' },
       params: {
+        category: code ? code : '',
         limit: 10,
         offset: number,
       },
@@ -83,7 +89,6 @@ function Restaurants() {
   }, [inView, loading])
 
   const PositionCalculation = (e, idx) => {
-    console.log(e.slice(0, -8), idx + 1)
     var geocoder = new kakao.maps.services.Geocoder();
     geocoder.addressSearch(e.slice(0, -8), function (result, status) {
       if (status === kakao.maps.services.Status.OK) {
@@ -118,7 +123,17 @@ function Restaurants() {
     var d = R * c; // Distance in km
     return d;
   }
-
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    autoplay: false,
+    autoplaySpeed: 90000,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    centerMode: false,
+    centerPadding: "20px"
+  };
   return (
     <Container>
       <h6>현재위치</h6>
@@ -143,13 +158,13 @@ function Restaurants() {
                 })}
               </div>
             ) : (
-              <div className='res-img-wrapper'>
+              <Slider {...settings}>
                 {data.images.map((img, i) => {
                   return (
                     <img src={decodeURIComponent(data.images[i].image.replace('http://127.0.0.1:8000/media/', ''))} className='res-img' />
                   )
                 })}
-              </div>
+              </Slider>
             )}
             <div style={{ paddingLeft: '10px' }}>
               <Link to={`/res_index/${data.id}`} className="res-index-name"><button className={styled.numberbtn}><p style={{ margin: '0' }}>{data.id}</p></button><button className={styled.categorynomargin}>{data.category_name}</button>
