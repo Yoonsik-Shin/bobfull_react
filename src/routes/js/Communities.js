1494d68486b697801f9c6aa5ff89ec9539961b4a
kmk4162 <kmk41623@gmail.com>
2022-12-12 17:41:01 +0900

    fix : 커뮤니티 페이지 디자인

--- src/routes/js/Communities.js
+++ src/routes/js/Communities.js
@@ -1,22 +1,23 @@
-import { Container, Form } from 'react-bootstrap'
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { Container, Form } from "react-bootstrap";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Topnavbar from '../../../src/components/js/Topnavbar';
-import CustomToggle from '../../../src/components/js/CustomArticle';
-import '../css/Community.css'
+import Topnavbar from "../../../src/components/js/Topnavbar";
+import CustomToggle from "../../../src/components/js/CustomArticle";
+import "../css/Community.css";
 import ContentCheck from "../../components/js/ArticleContent";
 import TitleCheck from "../../components/js/ArticleTitle";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import moment from "moment";
+import "moment/locale/ko";
 
-
-var baseURL = process.env.REACT_APP_BASE_URL
+var baseURL = process.env.REACT_APP_BASE_URL;
 
 function Community() {
-  const [articles, setArticles] = useState()
-  const [title, setTitle] = useState("")
-  const [content, setContent] = useState("")
+  const [articles, setArticles] = useState();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -29,77 +30,82 @@ function Community() {
   };
   const getArticle = async () => {
     const res = await axios({
-      method: 'get',
-      url: `${baseURL}/community/`
-    })
-    setArticles(res.data)
-  }
+      method: "get",
+      url: `${baseURL}/community/`,
+    });
+    setArticles(res.data);
+  };
   const onSubmit = async (e) => {
     e.preventDefault();
     const submit = await axios({
-      method: 'post',
+      method: "post",
       url: `${baseURL}/community/`,
       data: {
         title: title,
         content: content,
-      }
-    })
-    getArticle()
-    handleClose()
-  }
+      },
+    });
+    getArticle();
+    handleClose();
+  };
   useEffect(() => {
-    getArticle()
-  }, [])
+    getArticle();
+  }, []);
   return (
     <Container>
-      <Topnavbar
-        key='res'
-        pagename='커뮤니티'
-      />
-      <h2>글 목록</h2>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
+      <Topnavbar key="res" pagename="커뮤니티" />
+      <div className="d-flex justify-content-between community-titles">
+        <h2 className="article-list">글 목록</h2>
+        <Button variant="primary" className="articlebtn" onClick={handleShow}>
+          글 작성
+        </Button>
+      </div>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>작성하기</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={onSubmit}>
             <TitleCheck handleTitle={handleTitle} />
             <ContentCheck handleContent={handleContent} />
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button type='submit' variant="primary">
-              작성
-            </Button>
+            <div className="modal-btns">
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button type="submit" variant="primary" className="articlebtn">
+                작성
+              </Button>
+            </div>
           </Form>
         </Modal.Body>
       </Modal>
-      {
-        articles ?
-          <>
-            {articles.map((data, idx) => {
-              return (
-                <div>
-                  <h6>
-                    글번호 {data.pk}
-                    글제목 <Link to={`/community/${data.pk}?name=${data.pk}`}>
-                      {data.title}
-                    </Link>
-                    작성시간 {data.created_at}
-                    작성자 {data.user}
-                    댓글수 {data.comments.length}
-                  </h6>
-                </div>
-              )
-            })}
-          </>
-          : null
-      }
+      {articles ? (
+        <>
+          {articles.map((data, idx) => {
+            return (
+              <div className="article-card">
+                {/* <p>글번호 {data.pk}</p> */}
+                <p>{data.user}</p>
+                <p>
+                  <Link
+                    to={`/community/${data.pk}?name=${data.pk}`}
+                    className="article-title"
+                  >
+                    {data.title}
+                  </Link>{" "}
+                  [{data.comments.length}]
+                </p>
+                <p>
+                  작성시간{" "}
+                  {moment(data.created_at).format("YYYY년 MM월 D일 a h시mm분")}
+                </p>
+              </div>
+            );
+          })}
+        </>
+      ) : null}
     </Container>
-  )
+  );
 }
 
 export default Community;
