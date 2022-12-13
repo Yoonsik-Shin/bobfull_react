48c486971e2f9bdf6927b4e514d27b2f16c9d9df
Yoonsik-Shin <sys25800994@gmail.com>
2022-12-13 17:30:44 +0900

    feat : 채팅룸 구현중

--- src/routes/js/MatchingRoom.js
+++ src/routes/js/MatchingRoom.js
@@ -33,7 +33,6 @@ function MatchingRoom() {
     // setResId(matchingRoom.data[0].restaurant_id)
     // setResName(matchingRoom.data[0].restaurant_name)
   }
-
   useState(() => {
     getMatchingRoom();
   }, [matchList]);
@@ -43,15 +42,14 @@ function MatchingRoom() {
       <h2>매칭룸 리스트</h2>
       { matchList ? <div>{resId} {resName}</div> : null}
       <button onClick={()=>{setFormModal(!formModal)}}>매칭룸 생성하기</button>
-      {formModal ? <CreateForm getMatchingRoom={getMatchingRoom} responseChat={responseChat} setResponseChat={setResponseChat}/> : null}
-        
+      {formModal ? <CreateForm getMatchingRoom={getMatchingRoom} responseChat={responseChat} setResponseChat={setResponseChat} /> : null}
       {
         matchList ? 
         matchList.map((el, idx) => {
           return (
             <Card style={{marginBottom: '10px', marginTop: '10px'}} key={idx}>
               <Card.Body>
-                <Card.Title>{el.title}</Card.Title>
+                <Card.Title>#{el.id} {el.title}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">매칭룸 호스트 : {
                   el.user.profile_image ?
                   <img src={el.user.profile_image} alt="" width='30px' className="profile-img" />
@@ -63,7 +61,9 @@ function MatchingRoom() {
                 <div>매칭룸 내용 : {el.content}</div>
                 <div>약속날자/시간 : {el.to_date}</div>
                 <div>참여 멤버수 : {el.member.length}</div>
+                <div>채팅룸 번호 : {el.chatroom}</div>
                 <Link to={`/matching_room/${id}/${el.id}`}>자세히보기</Link>
+                <Link to={`/multichat/${el.id}`}>채팅룸</Link>
               </Card.Body>
             </Card>
           )
