117c1f1d8f3621fc1df342cee56bb1d83ad9d5a8
kmk4162 <kmk41623@gmail.com>
2022-12-14 10:58:39 +0900

    fix : 전체적 디자인 수정

--- src/routes/js/ChatRoomDetail.js
+++ src/routes/js/ChatRoomDetail.js
@@ -1,72 +1,86 @@
-import { Container, Form } from "react-bootstrap"
+import { Container, Form } from "react-bootstrap";
 import axios from "axios";
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
+import "../css/ChatRoomDetail.css";
 import { useState } from "react";
 import { useEffect } from "react";
-import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query' 
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 
-var baseURL = process.env.REACT_APP_BASE_URL
+var baseURL = process.env.REACT_APP_BASE_URL;
 
 function ChatRoomDetail() {
-  
-  const { room_id } = useParams()
-  const [inputChating, setInputChating] = useState()
+  const { room_id } = useParams();
+  const [inputChating, setInputChating] = useState();
   const handleInputChat = (e) => {
-    setInputChating(e.target.value)
-  }
-  const [messages, setMessages] = useState()
-  const interval = 2000
-  const getMessages = useQuery(['Messages'], ()=>
-    axios({
-      method: 'get',
-      url: `${baseURL}/multichat/${room_id}/`
-    })
-      .then((res)=>{
-        console.log(res.data)
-        return res.data
+    setInputChating(e.target.value);
+  };
+  const [messages, setMessages] = useState();
+  const interval = 2000;
+  const getMessages = useQuery(
+    ["Messages"],
+    () =>
+      axios({
+        method: "get",
+        url: `${baseURL}/multichat/${room_id}/`,
+      }).then((res) => {
+        console.log(res.data);
+        return res.data;
       }),
-    {refetchInterval : interval},
-    {cacheTime: interval},
-    {staleTime: interval}
-  )
-
+    { refetchInterval: interval },
+    { cacheTime: interval },
+    { staleTime: interval }
+  );
 
   return (
     <Container>
-      <div>채팅방 디테일</div>
-      <div>채팅내용</div>
-      { getMessages.isLoading && '로딩중' }
-      { getMessages.error && '에러남' }
-      { getMessages.data &&getMessages.data.map((el, idx) => {
-        return (
-          <div key={idx}>
-            <div>채팅내용 : {el.content} || 보낸사람 : {el.sender.nickname}</div>
-          </div>
-        )
-      }) }
-      <SendChat room_id={room_id} setInputChating={setInputChating} inputChating={inputChating} handleInputChat={handleInputChat}/>
+      <h2>채팅방</h2>
+      {getMessages.isLoading && "로딩중"}
+      {getMessages.error && "에러남"}
+      {getMessages.data &&
+        getMessages.data.map((el, idx) => {
+          return (
+            <div key={idx} className="chat-box">
+              <div className="chat-text">
+                <img
+                  src={`${el.sender.profile_image}`}
+                  className="profile-img"
+                ></img>
+                {el.sender.nickname} : {el.content}
+              </div>
+            </div>
+          );
+        })}
+      <SendChat
+        room_id={room_id}
+        setInputChating={setInputChating}
+        inputChating={inputChating}
+        handleInputChat={handleInputChat}
+      />
     </Container>
-  )
+  );
 }
 
-export default ChatRoomDetail
+export default ChatRoomDetail;
 
 function SendChat(props) {
-
   const sending = async (e) => {
     e.preventDefault();
     const sendingCheck = await axios({
-      method: 'post',
+      method: "post",
       url: `${baseURL}/multichat/${props.room_id}/send/`,
-      data: {content : props.inputChating}
-    }) 
-    console.log(sendingCheck.data)
-  } 
-  console.log(props.inputChating)
-  return(
-    <Form onSubmit={sending}>
+      data: { content: props.inputChating },
+    });
+    console.log(sendingCheck.data);
+  };
+  console.log(props.inputChating);
+  return (
+    <Form onSubmit={sending} className="send-form">
       <Form.Control type="text" onChange={props.handleInputChat}></Form.Control>
-      <button>보내기</button>
+      <button className="sendbtn">보내기</button>
     </Form>
-  )
+  );
 }
